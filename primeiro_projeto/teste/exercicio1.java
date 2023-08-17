633490d93f8313654a42070735d9b4495c240d51
edubardinis <eduardobardinis@gmail.com>
2023-08-17 19:28:11 -0300

    primeiro

---
+++ primeiro_projeto/teste/exercicio1.java
@@ -0,0 +1,18 @@
+package teste;
+
+public class exercicio1 {
+
+	public static void main(String[] args) {
+		
+		double n1, n2, soma;
+		
+		n1 = (10);
+		n2 = (30);
+		soma = n1 + n2;
+		
+		System.out.println("O valor da soma dos números é = " + soma); 
+		
+
+	}
+
+}
---
+++ primeiro_projeto/teste/exercicio2.java
@@ -0,0 +1,23 @@
+package teste;
+
+import java.util.Scanner;
+
+public class exercicio2 {
+
+	public static void main(String[] args) {
+		
+		double raio, raiz;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		raio = sc.nextDouble();
+		raiz = (3.14159 * raio);
+				
+		System.out.println("A raiz quadrada do raio é?");
+		System.out.println(raiz);
+		
+		
+		sc.close();
+	}
+
+}
